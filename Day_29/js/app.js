da2edd523f51e1f5825b9c07640af5e2aec8f90c
Nguyễn Trung Nguyên <31590813+NguyenKeo@users.noreply.github.com>
2023-09-19 00:13:55 +0700

    btvn b29 20

---
+++ Day_29/js/app.js
@@ -0,0 +1,9 @@
+var list = document.querySelector('.list');
+var item = document.querySelectorAll('.list-item');
+var modules = document.querySelectorAll('.active.list-item'); // class viết liền
+
+
+console.log('list: ', list);
+console.log('item: ', item);
+console.log('modules: ', modules);
+
