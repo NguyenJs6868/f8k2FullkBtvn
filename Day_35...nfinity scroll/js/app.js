fd16a2c159287340ccd2b6d231c0d5808619acbc
Nguyễn Trung Nguyên <31590813+NguyenKeo@users.noreply.github.com>
2023-10-13 01:35:25 +0700

    btvnB35 f8k2 infinity scroll push data goood

--- Day_35...nfinity scroll/js/app.js
+++ Day_35...nfinity scroll/js/app.js
@@ -1,16 +1,22 @@
 import { client } from "./client.js";
 
 const listCategoryEl = document.querySelector(".list-category");
-const loadingEl = document.getElementById("loading");
+const loadingEl = document.querySelector(".loading");
 
 let isLoadingData = false;
 let page = 1;
 let limit = 2; // Giới hạn
+// let limit = null;
 let start = 0; // Bắt đầu lấy từ bài viết vs index = 0
 
 // Lấy chiều cao của hàm hình
-const windowHeight = window.innerHeight;
+
+var windowHeight = window.innerHeight;
 console.log('windowHeight: ', windowHeight);
+
+document.body.style.minHeight = (windowHeight + 0.1) + 'px';
+
+let arrCategory = [];
 
 const getCategory = async () => {
     console.log('getCategory start: ', start);
@@ -22,7 +28,7 @@ const getCategory = async () => {
 
 
         // Gọi hàm Get list
-        const response = await client.get( `/categorys?_start=${start}&_limit=${limit}`);
+        const response = await client.get( `/categorys?_start=${start}&_limit=${limit ? limit : ''}`);
         console.log('getCategory res: ', response);
 
         const status = Number(response['response']['status']);
@@ -31,15 +37,23 @@ const getCategory = async () => {
         const { data } = response;
         console.log('getCategory data: ', data);
 
+        // Lấy tạm độ dài data trả all ntn vì tìm mãi ko thấy total đâu cả
+        const res = await client.get( `/categorys?_start=${0}&_limit=${''}`);
+
+
+        const lengthData = res.data.length;
+        console.log('getCategory lengthData: ', lengthData);
 
         if (status === 200) {
             // Lặp và in dữ liệu lên html
-            listCategoryEl.innerHTML =
-            `
-                ${data.map((item) =>
-                    `
+
+            console.log('arrCategory: ', arrCategory);
+            data.map((item) => {
+                arrCategory.push(
+                `
                     <div class="wrap-item">
                         <h3 class="title">${item.title}</h3>
+                        <div class="tag">${item.tag}</div>
                         <div class="item">
                             <img src="${item.thumbnail}" alt="${item.type[0]}">
                         </div>
@@ -47,15 +61,21 @@ const getCategory = async () => {
                             ${item.content}
                         </div>
                     </div>
-                    `
-                )}
-            `
+                `
+                );
+            });
 
-
-
+            listCategoryEl.innerHTML = arrCategory;
 
             start += limit; // Mỗi lần cho gọi bao nhiêu, sau khởi đầu gọi tăng lên bấy nhiêu
-            loadingEl.style.display = "none";
+
+
+            if (start < lengthData) { // Đạt độ dài của mảng data thì dừng không gọi nữa
+
+                loadingEl.classList.display = "none";
+
+                isLoadingData = false;
+            };
 
         }
 
@@ -69,20 +89,21 @@ getCategory(); // Gọi hiển thị lần đầu
 function handleScroll() {
 	console.log('Cuộn  1 khấc chuột gọi API GET -  Tăng số limit lên, event :');
 
-    const documentHeight = Math.max(
-        document.documentElement.scrollHeight,
-        document.body.scrollHeight
-        );
-        const scrollTop = Math.max(
-        window.pageYOffset,
-        document.documentElement.scrollTop,
-        document.body.scrollTop
-        );
+//     const windowHeight = window.innerHeight;
+//     console.log('windowHeight: ', windowHeight);
+//
+//     const documentHeight = Math.max(document.documentElement.scrollHeight, document.body.scrollHeight);
+//     const scrollTop = Math.max(
+//         window.pageYOffset,
+//         document.documentElement.scrollTop,
+//         document.body.scrollTop
+//     );
+//
+//     if (scrollTop + windowHeight >= documentHeight) {
+//         console.log("Đã cuộn tới cuối chiều cao màn hình");
+//     }
+    getCategory();
 
-        if (scrollTop + windowHeight >= documentHeight) {
-            console.log("Đã cuộn tới cuối chiều cao màn hình");
-            getCategory();
-        }
 }
 window.addEventListener("scrollend", handleScroll);
 
