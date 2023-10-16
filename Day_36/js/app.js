68c52488a4fbe77a9efe3aeaa9d7a4053cdc613b
Nguyễn Trung Nguyên <31590813+NguyenKeo@users.noreply.github.com>
2023-10-17 02:09:53 +0700

    btvn b36 quizz 20 start done

---
+++ Day_36/js/app.js
@@ -0,0 +1,76 @@
+/*
+ 1 Covid-19 xuất hiện lần đầu tiên ở nước nào ?
+ ["Viet Nam", "USA", "China", "India"]
+
+ 2 Thực hiện giãn cách xã hội tối thiểu trong bao nhiêu ngày ?
+ ["15 ngày", "7 ngày", "21 ngày", "1 tháng"]
+
+ 3 Con chó có mấy cái đùi
+ ["1 đùi", "2 đôi đùi", "2 đùi", "4 tay" ]
+
+ 4 Một con vịt có mấy cái cánh ?
+ ["1 cánh", "2 đôi cánh", "1 đôi cánh", "Không có"]
+
+ 5 Bạn sẽ làm được bài tập về nhà này chứ?
+ ["Không :v", "Có", "Dễ :v", "Thôi dẹp đi"]
+ require-text": "(Choose 2 correct answer(s))"
+
+ 6 Bạn có phải developer không?
+ ["Không", "Có", "Chắc chắn rồi", "Không bíc :v"]
+ require-text": "(Choose 2 correct answer(s))"
+
+ 7 Chim cánh cụt có mấy cánh?
+ ["Không có cánh", "2 chân", "2 tay", "2 cánh"]
+
+ 8 Khẩu hiệu 5K là gì ?
+ [Không đeo khẩu trang", Không tụ tập nơi đông người", Không ra đường", "Khoảng cách - Khẩu trang - Khử khuẩn - Khai báo y tế - Khử khuẩn"]
+
+*/
+
+// màn start click nút -> chán sự kiện đếm ngược vs số in vẻ text vào (Dùng hàm giống bài getlink chuyển trang nhừng đếm ngược)
+
+const quizizzGameEL = document.querySelector(".quizizzGame");
+const quizizzGamesStartEL = quizizzGameEL.querySelector(".quizizzGames-start");
+const quizizzGamesPlayEL = quizizzGameEL.querySelector(".quizizzGames-play");
+
+const btnStartEL = quizizzGameEL.querySelector(".btn-start");
+const downnTimeEL = quizizzGameEL.querySelector(".downn-time");
+
+const timeIKntervalStart = 3;
+
+btnStartEL.addEventListener("click", function() {
+    console.log('Click Start');
+
+    // ẩn nút start
+    btnStartEL.style.display = 'none';
+    downnTimeEL.style.display = "flex";
+
+    function countdown(seconds) {
+        let count = seconds;
+
+        const timer = setInterval(() => {
+            console.log(count);
+            count--;
+
+            if (count === 0) {
+                downnTimeEL.innerText = "GO"
+            }
+
+            if (count < 0) {
+                clearInterval(timer);
+                console.log("Đếm ngược hoàn thành!");
+
+                quizizzGamesStartEL.style.display = 'none';
+                quizizzGamesStartEL.remove();
+                quizizzGamesPlayEL.style.visibility = 'visible';
+
+            }
+        }, 1000);
+    }
+    countdown(timeIKntervalStart);
+
+
+});
+
+
+// -> remove màn start đi -> hiện màn PLay lên
---
+++ Day_36/js/config.js
@@ -0,0 +1,12 @@
+// Data Base Quizz
+// https://codesandbox.io/p/sandbox/modest-dawn-qn7kjz?file=%2Fpackage.json%3A17%2C22
+
+export const config = {
+  // SERVER_API: "http://localhost:3000",
+  SERVER_API: "https://qn7kjz-8080.csb.app",
+  PAGE_LIMIT: 1,
+};
+
+
+// quizz-questions
+// quizz-correct-answer
